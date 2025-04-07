95b7cba0665f46041ed0cddef829021563dbdeb4
Lee <120439693+lygitdata@users.noreply.github.com>
2025-04-07 22:25:43 +0200

    Add files via upload

---
+++ asset/js/footer.js
@@ -0,0 +1,23 @@
+document.addEventListener('DOMContentLoaded', function () {
+    var normal = document.getElementById('footer');
+    if (normal) {
+        var content = `
+            <div class="container text-center py-3 small border-top" style="margin-top: 100px;">
+                <p class="text-center" style="margin-top: 50px;"> &copy; <script type="text/JavaScript"> document.write(new Date().getFullYear()) </script> GarmentIQ.ly.gd.edu.kg </p>
+                <p class="text-center" xmlns:cc="http://creativecommons.org/ns#"> This webpage is licensed under <a href="https://creativecommons.org/licenses/by/4.0/?ref=chooser-v1" class="link-fancy" target="_blank" rel="license noopener noreferrer" style="display:inline-block;">Creative Commons Attribution 4.0 International <img style="height:22px!important;margin-left:3px;vertical-align:text-bottom;" src="https://mirrors.creativecommons.org/presskit/icons/cc.svg?ref=chooser-v1" alt="">
+                        <img style="height:22px!important;margin-left:3px;vertical-align:text-bottom;" src="https://mirrors.creativecommons.org/presskit/icons/by.svg?ref=chooser-v1" alt="">
+                    </a>
+                </p>
+                <p class="text-center">Last update of this website: 04/07/2025</p>
+                <p class="text-center">
+                    <a class="link-fancy" target="_blank" href="https://www.gd.edu.kg/privacy-policy/">Privacy Policy</a>
+                    <a class="link-fancy" target="_blank" href="https://www.gd.edu.kg/cookie-policy/">Cookie Policy</a>
+                </p>
+                <p class="text-center" style="margin-top: 50px; font-size: 8px;"> Website template provided by <a href="https://templatedeck.com" class="link-fancy" target="_blank">templatedeck.com</a>. </p>
+            </div>
+        `;
+        normal.innerHTML = content;
+    } else {
+        console.error('Container not found. Make sure the div with id "normalMenu" exists.');
+    }
+});
---
+++ asset/js/headerMenu.js
@@ -0,0 +1,27 @@
+document.addEventListener('DOMContentLoaded', function () {
+    var normal = document.getElementById('navbarSupportedContent');
+    if (normal) {
+        var content = `
+                    <ul class="navbar-nav me-auto mb-2 mb-lg-0" style="margin-left: auto; margin-right: 0;">
+                        <li class="nav-item">
+                            <a class="nav-link" href="/application/">Application</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="/">Paper</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="/">Documentation</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="https://github.com/lygitdata/GarmentIQ" target="_blank">GitHub</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="/dashboard/">Dashboard</a>
+                        </li>
+                    </ul>
+        `;
+        normal.innerHTML = content;
+    } else {
+        console.error('Container not found. Make sure the div with id "normalMenu" exists.');
+    }
+});
