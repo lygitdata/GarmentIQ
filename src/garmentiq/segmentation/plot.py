f989d43e4dc431693647e807feba427afbad36b0
Lee <120439693+lygitdata@users.noreply.github.com>
2025-04-13 23:07:05 +0200

    Update plot.py

--- src/garmentiq/segmentation/plot.py
+++ src/garmentiq/segmentation/plot.py
@@ -1,7 +1,7 @@
 import matplotlib.pyplot as plt
 import numpy as np
 
-def plot(image: np.ndarray, figsize: tuple = (6, 6)):
+def plot(image_np: np.ndarray, figsize: tuple = (6, 6)):
     """
     Displays an image using matplotlib, with optional customization of the figure size.
 
@@ -9,8 +9,8 @@ def plot(image: np.ndarray, figsize: tuple = (6, 6)):
     If the image is 2D (grayscale), it will use a grayscale colormap for visualization.
     It also provides an option to adjust the figure size via the `figsize` parameter.
 
-    :param image: The image to be displayed. Can be either 2D (grayscale) or 3D (color) numpy array.
-    :type image: numpy.ndarray
+    :param image_np: The image to be displayed. Can be either 2D (grayscale) or 3D (color) numpy array.
+    :type image_np: numpy.ndarray
     :param figsize: The size of the figure (width, height). Default is (6, 6).
     :type figsize: tuple, optional
 
@@ -23,9 +23,9 @@ def plot(image: np.ndarray, figsize: tuple = (6, 6)):
 
     # If the image is 2D (grayscale), use 'gray' colormap for a black & white mask
     if image.ndim == 2:
-        plt.imshow(image, cmap="gray")
+        plt.imshow(image_np, cmap="gray")
     else:
-        plt.imshow(image)
+        plt.imshow(image_np)
 
     # Remove axis and padding around the plot
     plt.axis("off")
