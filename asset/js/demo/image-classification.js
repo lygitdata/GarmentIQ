74267ff38152385afbe397c178ed89b46f2cbecb
Lee <120439693+lygitdata@users.noreply.github.com>
2025-04-14 00:03:19 +0200

    Update image-classification.js

--- asset/js/demo/image-classification.js
+++ asset/js/demo/image-classification.js
@@ -9,9 +9,9 @@ const classes = ['long sleeve dress', 'long sleeve top', 'short sleeve dress',
     'trousers', 'vest', 'vest dress'
 ];
 const modelChoices = {
-    "CNN-3": "https://github.com/lygitdata/GarmentIQ/raw/refs/heads/gh-pages/application/demo/image-classification/models/cnn_3.onnx",
-    "CNN-4": "https://github.com/lygitdata/GarmentIQ/raw/refs/heads/gh-pages/application/demo/image-classification/models/cnn_4.onnx",
-    "Tiny ViT": "https://github.com/lygitdata/GarmentIQ/raw/refs/heads/gh-pages/application/demo/image-classification/models/tiny_vit.onnx"
+    "CNN-3": "https://raw.githubusercontent.com/lygitdata/GarmentIQ/refs/heads/gh-pages/application/demo/image-classification/models/cnn_3.onnx",
+    "CNN-4": "https://raw.githubusercontent.com/lygitdata/GarmentIQ/refs/heads/gh-pages/application/demo/image-classification/models/cnn_4.onnx",
+    "Tiny ViT": "https://raw.githubusercontent.com/lygitdata/GarmentIQ/refs/heads/gh-pages/application/demo/image-classification/models/tiny_vit.onnx"
 };
 
 const tabsContainer = document.getElementById("model-tabs");
