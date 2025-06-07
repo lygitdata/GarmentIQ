d39a4d6e6a99113a878a303f19099801d2575353
Lee <120439693+lygitdata@users.noreply.github.com>
2025-06-07 22:44:04 +0200

    Update __init__.py

--- src/garmentiq/utils/__init__.py
+++ src/garmentiq/utils/__init__.py
@@ -3,3 +3,4 @@ from .unzip import unzip
 from .check_unzipped_dir import check_unzipped_dir
 from .check_filenames_metadata import check_filenames_metadata
 from .validate_garment_class_dict import validate_garment_class_dict
+from .compute_measurement_distances import compute_measurement_distances
