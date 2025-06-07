1f517881e9ff0922a84fe21d524d75aa81d9bc9a
Lee <120439693+lygitdata@users.noreply.github.com>
2025-06-07 22:09:18 +0200

    Update derive_keypoint_coord.py

--- src/garmentiq/landmark/derivation/derive_keypoint_coord.py
+++ src/garmentiq/landmark/derivation/derive_keypoint_coord.py
@@ -1,10 +1,5 @@
 from typing import Tuple, Optional
 import numpy as np
-
-from .load_and_extract_coord import (
-    _get_coordinates_from_json,
-    _get_required_coordinates,
-)
 from .line_intersect import _find_line_line_intersection
 from .mask_intersect import _get_mask_boundary, _find_line_mask_intersections
 from .utils import _calculate_line1_vector, _find_closest_point
