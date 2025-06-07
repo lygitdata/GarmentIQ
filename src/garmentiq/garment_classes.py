df5b068cf0adcb8ba95b59e5e17cc024f56e598f
Lee <120439693+lygitdata@users.noreply.github.com>
2025-06-07 17:48:50 +0200

    Add files via upload

--- src/garmentiq/garment_classes.py
+++ src/garmentiq/garment_classes.py
@@ -1,38 +1,47 @@
 garment_classes = {
     "long sleeve dress": {
         "num_predefined_points": 37,
-        "index_range": (219, 256)
+        "index_range": (219, 256),
+        "instruction": "instruction/long sleeve dress.json"
     }, 
     "long sleeve top": {
         "num_predefined_points": 33,
-        "index_range": (25, 58)
+        "index_range": (25, 58),
+        "instruction": "instruction/long sleeve top.json"
     }, 
     "short sleeve dress": {
         "num_predefined_points": 29,
-        "index_range": (190, 219)
+        "index_range": (190, 219),
+        "instruction": "instruction/short sleeve dress.json"
     }, 
     "short sleeve top": {
         "num_predefined_points": 25,
-        "index_range": (0, 25)
+        "index_range": (0, 25),
+        "instruction": "instruction/short sleeve top.json"
     }, 
     "shorts": {
         "num_predefined_points": 10,
-        "index_range": (158, 168)
+        "index_range": (158, 168),
+        "instruction": "instruction/shorts.json"
     }, 
     "skirt": {
         "num_predefined_points": 8,
-        "index_range": (182, 190)
+        "index_range": (182, 190),
+        "instruction": "instruction/skirt.json"
     }, 
     "trousers": {
         "num_predefined_points": 14,
-        "index_range": (168, 182)
+        "index_range": (168, 182),
+        "instruction": "instruction/trousers.json"
     }, 
     "vest": {
         "num_predefined_points": 15,
-        "index_range": (128, 143)
+        "index_range": (128, 143),
+        "instruction": "instruction/vest.json"
     }, 
     "vest dress": {
         "num_predefined_points": 19,
-        "index_range": (256, 275)
+        "index_range": (256, 275),
+        "instruction": "instruction/vest dress.json"
     }
 }
