091def1d33146bbdd5f617f90d293b2fdba2a8bf
Lee <120439693+lygitdata@users.noreply.github.com>
2025-06-07 18:05:07 +0200

    Update __init__.py

--- src/garmentiq/landmark/__init__.py
+++ src/garmentiq/landmark/__init__.py
@@ -1,9 +1,9 @@
 # garmentiq/landmark/__init__.py
 from .extraction.extraction_core import extraction_core as extraction 
 from .extraction import (
-	load_model
-	model_definition
-	utils
+	load_model,
+	model_definition,
+	utils,
 )
 from .derivation import *
 from .refinement import *
