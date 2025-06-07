75010974936cd885772bd87e8e003f347c8942a5
Lee <120439693+lygitdata@users.noreply.github.com>
2025-06-07 18:26:28 +0200

    Update __init__.py

--- src/garmentiq/landmark/extraction/__init__.py
+++ src/garmentiq/landmark/extraction/__init__.py
@@ -1,7 +1,7 @@
 # garmentiq/landmark/extraction/__init__.py
 from .load_model import load_model
 from .model_definition import PoseHighResolutionNet
-from .extraction_core import extraction_core
+from .process import process
 from .utils import (
     get_max_preds,
     get_final_preds,
