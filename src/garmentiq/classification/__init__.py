2a16f1cb7eea4063b5d308d6155a67de30268a40
Lee <120439693+lygitdata@users.noreply.github.com>
2025-06-08 12:30:37 +0200

    Update __init__.py

--- src/garmentiq/classification/__init__.py
+++ src/garmentiq/classification/__init__.py
@@ -1,6 +1,7 @@
 # garmentiq/classification/__init__.py
 from .train_test_split import train_test_split
 from .load_data import load_data
+from .load_model import load_model
 from .train_pytorch_nn import train_pytorch_nn
 from .fine_tune_pytorch_nn import fine_tune_pytorch_nn
 from .test_pytorch_nn import test_pytorch_nn
