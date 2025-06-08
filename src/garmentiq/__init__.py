66c5a5e4308a0d5b7da85097c6bc43c7fc7bc3d1
Lee <120439693+lygitdata@users.noreply.github.com>
2025-06-08 16:46:22 +0200

    Update __init__.py

--- src/garmentiq/__init__.py
+++ src/garmentiq/__init__.py
@@ -1,6 +1,6 @@
 # garmentiq/__init__.py
-from garmentiq import tailor
-from garmentiq import utils
-from garmentiq import classification
-from garmentiq import segmentation
-from garmentiq import landmark
+from .tailor import tailor
+from . import utils
+from . import classification
+from . import segmentation
+from . import landmark
